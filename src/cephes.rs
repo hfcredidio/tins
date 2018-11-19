e6c0846d2bfdc0825fcf67616cd2ad1f6a806281
heitor <heitor@redbaron.local>
2018-11-19 08:04:09 +0000

    Wrap cephes airy

--- src/cephes.rs
+++ src/cephes.rs
@@ -1,9 +1,17 @@
 mod _cephes {
     extern "C" {
-        pub fn i0(x: f64) -> f64;
+        pub fn airy(x: f64, ai: &mut f64, aip: &mut f64, bi: &mut f64, bip: &mut f64) -> i32;
     }
 }
 
-pub fn i0(x: f64) -> f64 {
-    unsafe { _cephes::i0(x) }
+pub fn airy(x: f64) -> (f64, f64, f64, f64) {
+    let mut ai = 0.0f64;
+    let mut bi = 0.0f64;
+    let mut aip = 0.0f64;
+    let mut bip = 0.0f64;
+    let err = unsafe { _cephes::airy(x, &mut ai, &mut bi, &mut aip, &mut bip) };
+    if err == -1 {
+        eprintln!("cephes.airy: value of x too large");
+    }
+    (ai, bi, aip, bip)
 }
--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,7 @@ mod cephes;
 mod complex;
 mod specfun;
 
-use crate::amos::*;
-use crate::cdflib::*;
-use crate::cephes::*;
-use crate::specfun::*;
+
 
 fn main() {
-    println!("{}", airy_a(1 as f64));
-    println!("{}", der_airy_a(1 as f64));
-    println!("{}", airy_b(1 as f64));
-    println!("{}", der_airy_b(1 as f64));
-    println!("{:?}", airy_a_zeros(3));
-    println!("{:?}", airy_b_zeros(3));
-    println!("{:?}", it_airy(3.0));
 }
